bae375065850f2ceb63017a151b88252b8f99ffa
iamjihyun <jhn_05@naver.com>
2023-01-20 15:29:18 +0900

    20230120 산성비 단어 랜덤생성을 위한 맵 생성

--- fnGameBoard/src/app/rainingWordsGame/raining-words-game/raining-words-game.component.ts
+++ fnGameBoard/src/app/rainingWordsGame/raining-words-game/raining-words-game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Word } from '../word';
 import { WORDS } from "../word-list";
+
 // import { NgRedux, select } from '@angular-redux/store';
 
 @Component({
@@ -10,75 +11,74 @@ import { WORDS } from "../word-list";
   styleUrls: ['./raining-words-game.component.css']
 })
 
-
 export class RainingWordsGameComponent implements OnInit {
-
-  public ab: string | undefined;
-  public score: string = "점수 : ";
-  public life: string = "life : ";
-  // public word: any[] = ["fifa", "maple", "lol", "battleground", "철권", "던전앤파이터", "카트라이더", "포켓몬스터", "디지몬", "짱구",
-  // "도라에몽", "아따맘마", "SuddenAttack", "검은사막", "Tera", "원피스", "나루토", "마비노기", "로스트아크", "블리치"
-  // ];
-
-constructor() { }
-
-ngOnInit(): void {
-
-    // taja배열의 index 값에 대한 변수
-    var idx = 0;
-
-    // 랜덤으로 배열을 섞어주기 위한 for문
-    var randomPick = 0;
-    var temp = null;
-
-    // for (let i = 0; i < this.word.length; i++) {
-    //   randomPick = Math.round(Math.random() * (this.word.length - 1));
-    //   temp = this.word[randomPick];
-    //   this.word[randomPick] = this.word[i];
-    //   this.word[i] = temp;
-    // }
+  //뷰차일드
   
+  //변수선언
+  public mapArr:any = [];
+  public ab: string | undefined;
+  public score: string = "SCORE : ";
+  public life: string = "LIFE : ";
+  public time: string = "TIME : ";
+  public DOWNTIME = 2000;
+  //생성자
+  constructor() {}
 
-  }
+  ngOnInit(): void {
+  
+      // taja배열의 index 값에 대한 변수
+      var idx = 0;
+  
+      // 랜덤으로 배열을 섞어주기 위한 for문
+      var randomPick = 0;
+      var temp = null;
+  
+      // for (let i = 0; i < this.word.length; i++) {
+      //   randomPick = Math.round(Math.random() * (this.word.length - 1));
+      //   temp = this.word[randomPick];
+      //   this.word[randomPick] = this.word[i];
+      //   this.word[i] = temp;
+      // }
+    }
 
+    ngAfterViewInit():void {
+    
+    }
+
+    getDisplayTime(time: number): string {
+      const second:number = time % 60;
+      const minute:number = Math.floor(time / 60);
+      const sec: string = String(second).length === 2 ? String(second) : "0" + second;
+      const min: string = String(minute).length === 2 ? String(minute) : "0" + minute;
+      const displayTime: string = `${min}:${sec}`;
+      return displayTime;
+    }
 
   onClickStartBtn() {
-    // console.log("테스트");
+    console.log("시작");
+    console.log(this.mapArr);
+    let idx : number = Number((Math.random()*9).toFixed());
+    console.log(WORDS[idx].text);
+    // console.log('째깍');
+    this.ab = WORDS[idx].text;
     
-    setTimeout(() => { // 화면이 열리는 시간이 있으므로 setTimeout 으로 300ms 이 후에 실행하도록 한다.
-      // this.getRandomInt(4);
-      // console.log(this.getRandomInt(3));
-      // expected output: 0, 1 or 2
-      // let idx = this.getRandomInt(3);
-      // aa = this.word[idx];
-
-      // expected output: 0
-      
-      console.log(Math.random());
-      
-      // expected output: a number from 0 to <1
-    }, 300);
-    const DOWNTIME = 2000;
+    console.log(Math.random());
     
-    // let idx = this.getRandomInt(10);
-    // let timerId = setTimeout(function tick(this: any) {
-      /** setInterval을 이용하지 않고 아래와 같이 중첩 setTimeout을 사용함
-       let timerId = setInterval(() => alert('째깍'), 2000);
-       */
-        let idx : number = Number((Math.random()*9+1).toFixed());
-        console.log(idx);
-        console.log(WORDS[idx].text);
-        // console.log('째깍');
-        this.ab = WORDS[idx].text;
-
-
-        // timerId = setTimeout(tick, DOWNTIME); // (*)
-      // }, DOWNTIME);
-
-  
-  }
-
-  getRandomInt(max: any) {
-    return Math.floor(Math.random() * max);
-  }
+    
+    
+    
+    let count = 1;
+    const timeoutId = setInterval(() => {
+      
+      let oneLine:any = [10];
+      let mapIdx : number = Number((Math.random()*9).toFixed());
+      oneLine[mapIdx] = WORDS[idx].text;
+      console.log(count); // 1 2 3 4 5
+      this.mapArr.push(oneLine);
+      
+        if (count++ === 5) clearInterval(timeoutId);
+      }, 1000);
+      
+      console.log("timeoutId: ", timeoutId);
+}
 }
