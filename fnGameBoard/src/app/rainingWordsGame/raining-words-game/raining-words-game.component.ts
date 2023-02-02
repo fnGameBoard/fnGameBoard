cab44e7c3bd7b27a6697acda679a5c83255edd02
iamjihyun <jhn_05@naver.com>
2023-02-02 21:04:59 +0900

    20230202 입력값 관련

--- fnGameBoard/src/app/rainingWordsGame/raining-words-game/raining-words-game.component.ts
+++ fnGameBoard/src/app/rainingWordsGame/raining-words-game/raining-words-game.component.ts
@@ -17,12 +17,12 @@ export class RainingWordsGameComponent implements OnInit {
   //뷰차일드
   
   //변수선언
-  public num :any | undefined;
+  public answerWord :string | undefined;//정답입력값
   public mapArr:any = [];
   public ab: string | undefined;
   public score: string = "SCORE : ";
   public life: string = "LIFE : ❤️❤️❤️❤️❤️";
-  public time: string = "TIME : ";
+  public time: string = "TIME : 00:00:00";
   public timeNum: number = 0;
   public DOWNTIME = 2000;
 
@@ -30,98 +30,92 @@ export class RainingWordsGameComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+  }
+  ngAfterViewChecked(): void {
+  }
+  timer(timeNum: number): void {
+    // let startDate1: Date = new Date();       ///꼭 Date형식으로 변환 후 사용합니다. 
+    // let endDate1: Date = new Date();           ///꼭 Date형식으로 변환 후 사용합니다. 
+    // let diff = endDate1.getTime() - startDate1.getTime();
+    // this.time2 = ;
 
-      // taja배열의 index 값에 대한 변수
-      var idx = 0;
-  
-      // 랜덤으로 배열을 섞어주기 위한 for문
-      var randomPick = 0;
-      var temp = null;
-  
-      // for (let i = 0; i < this.word.length; i++) {
-      //   randomPick = Math.round(Math.random() * (this.word.length - 1));
-      //   temp = this.word[randomPick];
-      //   this.word[randomPick] = this.word[i];
-      //   this.word[i] = temp;
-      // }
-    }
+    var hour, min, sec;
 
-    ngAfterViewInit():void {
-    
-    }
-    ngAfterViewChecked(): void {
-    }
-    timer(timeNum:number):void{
-      let startDate1: Date = new Date();       ///꼭 Date형식으로 변환 후 사용합니다. 
-      let endDate1: Date = new Date();           ///꼭 Date형식으로 변환 후 사용합니다. 
-      let diff = endDate1.getTime() - startDate1.getTime();
-      // this.time2 = ;
+    var timer = setInterval(() => {
+      timeNum++; // 1초마다 증가, 타이머의 경우 time--;
 
-      var hour, min, sec;
+      min = Math.floor(timeNum / 60);
+      hour = Math.floor(min / 60);
+      sec = timeNum % 60;
+      min = min % 60;
 
-      var timer = setInterval(() =>{
-          timeNum++; // 1초마다 증가, 타이머의 경우 time--;
-  
-          min = Math.floor(timeNum/60);
-          hour = Math.floor(min/60);
-          sec = timeNum%60;
-          min = min%60;
-  
-          let th = hour.toString();
-          let tm = min.toString();
-          let ts = sec.toString();
-          
-          // 한자리일 경우 처리
-          if(hour < 10){
-              th = "0" + hour;
-          }
-          if(min < 10){
-              tm = "0" + min;
-          }
-          if(sec < 10){
-              ts = "0" + sec;
-          }
-  
-          // 함수 호출 당시 받은 object의 html 교체
-          this.time = "TIME : " + th + ":" + tm + ":" + ts;
+      let th = hour.toString();
+      let tm = min.toString();
+      let ts = sec.toString();
+
+      // 한자리일 경우 처리
+      if (hour < 10) {
+        th = "0" + hour;
       }
-  , 1000);
+      if (min < 10) {
+        tm = "0" + min;
+      }
+      if (sec < 10) {
+        ts = "0" + sec;
+      }
 
+      // 함수 호출 당시 받은 object의 html 교체
+      this.time = "TIME : " + th + ":" + tm + ":" + ts;
+    }
+      , 1000);
+  }
 
-    }
-    getDisplayTime(time: number): string {
-      const second:number = time % 60;
-      const minute:number = Math.floor(time / 60);
-      const sec: string = String(second).length === 2 ? String(second) : "0" + second;
-      const min: string = String(minute).length === 2 ? String(minute) : "0" + minute;
-      const displayTime: string = `${min}:${sec}`;
-      return displayTime;
-    }
+    // getDisplayTime(time: number): string {
+    //   const second:number = time % 60;
+    //   const minute:number = Math.floor(time / 60);
+    //   const sec: string = String(second).length === 2 ? String(second) : "0" + second;
+    //   const min: string = String(minute).length === 2 ? String(minute) : "0" + minute;
+    //   const displayTime: string = `${min}:${sec}`;
+    //   return displayTime;
+    // }
 
   onClickStartBtn() {
-    console.log(this.num);
+
     //시간
-    let startDate1: Date = new Date();       ///꼭 Date형식으로 변환 후 사용합니다. 
     this.timer(this.timeNum);
 
     let count = 1;
     const timeoutId = setInterval(() => {
       let idx : number = Number((Math.random()*9).toFixed());
       this.ab = WORDS[idx].text;
-      let endDate1: Date = new Date();           ///꼭 Date형식으로 변환 후 사용합니다. 
-      let diff = endDate1.getTime() - startDate1.getTime();
-      // this.time = this.getDisplayTime(diff).toString();
       
-
-      let oneLine:any = [10];
+      let oneLine:any = [];
       let mapIdx : number = Number((Math.random() * 9).toFixed());
       oneLine[mapIdx] = WORDS[idx].text;
       console.log(count); // 1 2 3 4 5
       this.mapArr.push(oneLine);
       
-        if (count++ === 5) clearInterval(timeoutId);
-      }, 1000);
+        if (count++ === 20) clearInterval(timeoutId);
+      }, 500);
 
-      console.log("timeoutId: ", timeoutId);
+      // console.log("timeoutId: ", timeoutId);
+  }
+
+  //입력버튼을 누르면
+  answerBtn(){
+    console.log(this.answerWord);
+    console.log(this.mapArr);
+    if(this.answerWord !=undefined){
+      for(let i = 0;i<this.mapArr.length; i++){
+        for(let j = 0; j<this.mapArr[i].length; j++){
+          if(this.mapArr[i][j] == this.answerWord){
+            console.log("정답 : " + i +", "+ j);
+            console.log("정답 : " + this.mapArr[i][j]);
+          }
+        }
+      }
+    }
   }
 }
